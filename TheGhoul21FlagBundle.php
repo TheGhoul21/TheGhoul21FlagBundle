4602fc1aab72af5a1668360b84d1c51a6b7579c5
Luca Simonetti <luca.simonetti@nohup.it>
2018-07-06 10:28:26 +0200

    first commit

---
+++ TheGhoul21FlagBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Luca Simonetti <little.che90@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TheGhoul21\Bundle\FlagTypeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * FlagTypeBundle.
+ *
+ * @author Luca Simonetti <little.che90@gmail.com>
+ */
+class FlagTypeBundle extends Bundle
+{
+}
